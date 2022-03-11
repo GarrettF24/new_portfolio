6a7076aed5ed7b1e99120589a1ccd37b70bdf2b7
GarrettF <grfoster24@gmail.com>
2022-03-11 11:34:40 -0500

    checking to see if netlify is updating

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div id='home' className='App'>
       <Nav />
+      <h1>Hello</h1>
       <div className='app-container'>
         <div className='picture'>
           <img src={HEADSHOT} />
